a6fc987c28add9bc013878b68039a7bddc0105b7
Nick Hrones <nhrones@users.noreply.github.com>
2022-01-28 14:28:54 -0500

    removed serviceworker

--- public/app.js
+++ public/app.js
@@ -19,7 +19,9 @@ socket.when(socket.topic.SetID, (data) => {
     Players.setCurrentPlayer(Players.thisPlayer);
     socket.registerPlayer(data.id, name);
     Players.addPlayer(data.id, name);
-    game.resetGame();
+    if (game) {
+        game.resetGame();
+    }
 });
 socket.when(socket.topic.GameFull, () => {
     const msg = `Sorry, This game is already full!
@@ -30,11 +32,6 @@ This tab/window will automatically close!`;
     self.close();
 });
 self.addEventListener('DOMContentLoaded', () => {
-    navigator.serviceWorker.register('./public/sw.js').then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, (err) => {
-        console.log('ServiceWorker registration failed: ', err);
-    });
     Container.init(document.getElementById('canvas'), 'snow');
     DiceGame.init();
     container.hydrateUI();
