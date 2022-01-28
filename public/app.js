624dad6b000e34d77b4e9863ece691e282108b7b
Nick Hrones <nhrones@users.noreply.github.com>
2022-01-28 05:45:45 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -30,7 +30,7 @@ This tab/window will automatically close!`;
     self.close();
 });
 self.addEventListener('DOMContentLoaded', () => {
-    navigator.serviceWorker.register('./sw.js').then((registration) => {
+    navigator.serviceWorker.register('sw.js').then((registration) => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, (err) => {
         console.log('ServiceWorker registration failed: ', err);
