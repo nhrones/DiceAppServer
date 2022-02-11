f1d20dcb4a7e3e330bef88fbc67c15f5d6a48568
Nick Hrones <nhrones@users.noreply.github.com>
2022-02-10 22:51:07 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,11 +2,17 @@ import { DiceGame, game } from './model/diceGame.js';
 import { Container, container } from './view/container.js';
 import * as socket from './framework/model/signalling.js';
 import * as Players from './model/players.js';
-const { message } = socket;
+const { onSignalRecieved, registerPlayer, message } = socket;
 const proto = (window.location.protocol === 'http:') ? 'ws://' : 'wss://';
-export const serverURL = `${proto}${window.location.host}:8000`;
-socket.initialize('wss://rtc-signal-server.deno.dev');
-socket.onSignalRecieved(message.SetID, (data) => {
+const serverURL = `${proto}${window.location.host}:8000`;
+const thisHost = window.location.host;
+if (thisHost === 'localhost' || thisHost === '127.0.0.1') {
+    socket.initialize(serverURL);
+}
+else {
+    socket.initialize('wss://rtc-signal-server.deno.dev');
+}
+onSignalRecieved(message.SetID, (data) => {
     const name = 'Player';
     const hiddenButton = document.getElementById('hidden-button');
     hiddenButton.hidden = true;
@@ -18,13 +24,13 @@ socket.onSignalRecieved(message.SetID, (data) => {
     Players.thisPlayer.playerName = (name === 'Player') ? 'Player1' : name;
     Players.setThisPlayer(Players.thisPlayer);
     Players.setCurrentPlayer(Players.thisPlayer);
-    socket.registerPlayer(data.id, name);
+    registerPlayer(data.id, name);
     Players.addPlayer(data.id, name);
     if (game) {
         game.resetGame();
     }
 });
-socket.onSignalRecieved(message.GameFull, () => {
+onSignalRecieved(message.GameFull, () => {
     const msg = `Sorry, This game is already full!
 This tab/window will automatically close!`;
     console.log(msg);
